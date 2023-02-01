91c625c2b6b795b1c239acdd6e0c2ee0d7cba842
Sony MelliFriady <zoenasun@gmail.com>
2023-02-01 23:46:36 +0700

    [sony] create getProductById

--- backend/app/controllers/product.js
+++ backend/app/controllers/product.js
@@ -1,14 +1,35 @@
 const Product = require("../models/productModel");
+const { isValid } = require("mongoose").Types.ObjectId;
 
 // @desc    Fetch all products
-// @route   GET /api/products
+// @route   GET /api/v1/products
 // @access  Public
-async function getAll(req, res, next) {
+async function getProducts(req, res, next) {
   try {
-    const product = await Product.find().limit(10);
-    res.status(200).json(product);
+    const products = await Product.find().limit(10);
+    res.status(200).json(products);
   } catch (error) {
     next(error);
   }
 }
-module.exports = { getAll };
+
+// @desc    Fetch single product
+// @route   GET /api/v1/products/:productId
+// @access  Public
+async function getProductById(req, res, next) {
+  try {
+    const id = req.params.productId;
+
+    const isId = isValid(id);
+    if (!isId) throw { name: "InvalidMongoId" };
+
+    const product = await Product.findById(id).select("-createdAt -updatedAt -__v");
+
+    res.status(200).json(product);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+}
+
+module.exports = { getProducts, getProductById };
