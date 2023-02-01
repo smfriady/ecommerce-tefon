9031c2877104b3c4801e8ccb62b01294ac68c35b
Sony MelliFriady <zoenasun@gmail.com>
2023-02-02 00:50:40 +0700

    [sony] create router customers

--- backend/app/routes/index.js
+++ backend/app/routes/index.js
@@ -2,7 +2,9 @@ const API_URI = "/api/v1";
 
 const router = require("express").Router();
 const productRouter = require("./product");
+const customerRouter = require("./customer");
 
 router.use(`${API_URI}/products`, productRouter);
+router.use(`${API_URI}/customers`, customerRouter);
 
 module.exports = router;
