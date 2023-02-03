90de2a909f2826ef2a24b0fdea31762996c4cfd8
Sony MelliFriady <zoenasun@gmail.com>
2023-02-03 14:30:16 +0700

    [sony] create component Footer

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="bg-black text-sm text-center text-white py-3 w-full">
+      COPYRIGHT 2023- {new Date().getFullYear()} teFon. ALL RIGHTS RESERVED.
+    </div>
+  );
+};
+
+export default Footer;
