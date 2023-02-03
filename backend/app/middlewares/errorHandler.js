cff945baf2fede6abbacc769f628be891b7e06b4
Sony MelliFriady <zoenasun@gmail.com>
2023-02-03 13:17:41 +0700

    [sony] update errorHandler

--- backend/app/middlewares/errorHandler.js
+++ backend/app/middlewares/errorHandler.js
@@ -5,12 +5,10 @@ const errorHandler = (err, req, res, next) => {
   if (err.name === "InvalidMongoId") {
     code = 400;
     message = "Invalid product id.";
-  }
-  if (err.name === "UserExists") {
+  } else if (err.name === "UserExists") {
     code = 400;
     message = "User already exists.";
-  }
-  if (err.name === "InvalidEmailOrPassword") {
+  } else if (err.name === "InvalidEmailOrPassword") {
     code = 401;
     message = "Invalid email or password.";
   } else {
