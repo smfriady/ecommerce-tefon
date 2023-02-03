e9e6c80f12a68f91b872c6245f83468ff63af8fb
Sony MelliFriady <zoenasun@gmail.com>
2023-02-03 20:48:19 +0700

    [sony] update navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,7 +1,25 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { FaUser } from "react-icons/fa";
+import { useDispatch } from "react-redux";
+import { toast } from "react-toastify";
 
-const Navbar = () => {
+import { customerLogout } from "@/features/actions/authActions";
+
+const Navbar = ({ token }) => {
+  const dispatch = useDispatch();
+  const router = useRouter();
+
+  const handleLogout = (e) => {
+    e.preventDefault();
+    dispatch(customerLogout())
+      .then(({ message }) => {
+        router.push("/");
+        toast.success(message);
+      })
+      .catch((message) => toast.error(message));
+  };
+
   return (
     <div className="fixed-top">
       <nav className="bg-black flex">
@@ -12,22 +30,35 @@ const Navbar = () => {
             </Link>
           </div>
           <ul className="inline-flex space-x-2">
-            <li>
-              <Link
-                href="/login"
-                className="space-x-1 flex flex-row items-center justify-center"
-              >
-                <FaUser /> <span>login</span>
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/register"
-                className="space-x-1 flex flex-row items-center justify-center"
-              >
-                <FaUser /> <span>register</span>
-              </Link>
-            </li>
+            {token ? (
+              <li>
+                <button
+                  className="space-x-1 flex flex-row items-center justify-center"
+                  onClick={handleLogout}
+                >
+                  <FaUser /> <span>Logout</span>
+                </button>
+              </li>
+            ) : (
+              <>
+                <li>
+                  <Link
+                    href="/login"
+                    className="space-x-1 flex flex-row items-center justify-center"
+                  >
+                    <FaUser /> <span>login</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/register"
+                    className="space-x-1 flex flex-row items-center justify-center"
+                  >
+                    <FaUser /> <span>register</span>
+                  </Link>
+                </li>
+              </>
+            )}
           </ul>
         </div>
       </nav>
