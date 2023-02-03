4a3a111a02561fb15135c26f36b9d461c091d0c0
Sony MelliFriady <zoenasun@gmail.com>
2023-02-04 04:30:39 +0700

    [sony] new router POST and PUT products

--- backend/app/routes/product.js
+++ backend/app/routes/product.js
@@ -1,6 +1,16 @@
 const router = require("express").Router();
-const { getProducts, getProductById } = require("../controllers/product");
+const {
+  getProducts,
+  getProductById,
+  insertProduct,
+  updateProduct,
+} = require("../controllers/product");
+const { authentication, admin } = require("../middlewares/authentication");
 
 router.get("/", getProducts);
 router.get("/:productId", getProductById);
+
+router.use(authentication);
+router.post("/", admin, insertProduct);
+router.put("/:productId", admin, updateProduct);
 module.exports = router;
