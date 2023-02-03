969dfa0218ef52dfbb22c78eab7ec6d55db1dd74
Sony MelliFriady <zoenasun@gmail.com>
2023-02-03 20:51:51 +0700

    [sony] fetching products

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,11 +1,41 @@
-const Home = () => {
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import BaseLayout from "@/components/BaseLayout";
+import { cookieParser } from "@/helpers/cookieParser";
+import { getProducts } from "@/features/actions/productActions";
+import CardProduct from "@/components/CardProduct";
+
+const Home = ({ cookies }) => {
+  const dispatch = useDispatch();
+  const { loading, products, error } = useSelector((s) => s.products);
+
+  useEffect(() => {
+    dispatch(getProducts());
+  }, [dispatch]);
   return (
-    <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem eveniet quis quo
-      repellat iste esse molestias, illum numquam ratione temporibus eum beatae, a
-      reprehenderit soluta atque quasi eligendi omnis hic.
-    </div>
+    <BaseLayout title="Home Page" token={cookies ? true : false}>
+      <div className="container mx-auto w-full m-5 px-2">
+        <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-4 gap-2">
+          {products && products.map((p, i) => <CardProduct key={i} products={p} />)}
+        </div>
+      </div>
+    </BaseLayout>
   );
 };
+
+export async function getServerSideProps(context) {
+  const { token } = cookieParser(context.req);
+
+  if (!token) {
+    return {
+      props: {},
+    };
+  }
+  return {
+    props: {
+      cookies: token,
+    },
+  };
+}
 
 export default Home;
