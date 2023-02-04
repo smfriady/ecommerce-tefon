5562dec1f2bcda9edef4d9099a869bc1b92554cf
Sony MelliFriady <zoenasun@gmail.com>
2023-02-04 07:24:13 +0700

    [sony] update CardProduct

--- frontend/src/components/CardProduct.js
+++ frontend/src/components/CardProduct.js
@@ -1,12 +1,29 @@
+import Link from "next/link";
+import { useDispatch } from "react-redux";
 import { formatRupiah } from "@/helpers/formatRupiah";
 import { slugify } from "@/helpers/slug";
-import Link from "next/link";
+import { deleteProduct } from "@/features/actions/productActions";
+import { toast } from "react-toastify";
+import { useRouter } from "next/router";
+import FormCreateProduct from "./FormProduct";
 
-const CardProduct = ({ products }) => {
+const CardProduct = ({ products, token, isAdmin }) => {
   const { _id, product_name, product_price, brand, product_image_url, product_info } =
     products;
 
   let regex = /[,-]/g;
+
+  const router = useRouter();
+  const dispatch = useDispatch();
+
+  const handleDelete = (data) => {
+    dispatch(deleteProduct(data))
+      .then(({ message }) => {
+        router.push("/");
+        toast.success(message);
+      })
+      .catch((message) => toast.error(message));
+  };
 
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
@@ -15,7 +32,7 @@ const CardProduct = ({ products }) => {
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2">{`${product_name}`.split(regex)[0]}</div>
         <span className="inline-block bg-blue-600 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2">
-          {product_info}
+          {`${product_info === 0 ? "Stok Habis" : "Click & Pickup"}`}
         </span>
       </div>
       <div className="px-6">
@@ -26,6 +43,15 @@ const CardProduct = ({ products }) => {
         >
           Lihat Detail
         </Link>
+        {isAdmin && (
+          <button
+            onClick={() => handleDelete({ id: _id, token })}
+            className="inline-block bg-red-600 rounded-full px-3 py-1 text-sm font-semibold text-white mr-2"
+          >
+            Delete
+          </button>
+        )}
+        {isAdmin && <FormCreateProduct btnType="edit" products={products} token={token} />}
       </div>
       <div className="px-6 pt-2 pb-2">
         <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
